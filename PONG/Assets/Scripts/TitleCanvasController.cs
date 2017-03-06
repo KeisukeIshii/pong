81104b49b15a8df203ad3c2ed1ad939095038e47
kishii <ishiikeisukesyuukatsu@gmail.com>
2017-03-06 23:16:01 +0900

    [Game]遷移確認、BuildSettingにTitle、Gameを追加

--- PONG/Assets/Scripts/TitleCanvasController.cs
+++ PONG/Assets/Scripts/TitleCanvasController.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
 public class TitleCanvasController : MonoBehaviour {
 	
-	private Scene gameScene = SceneManager.GetSceneByName("Game");
-
 	/// <summary>ボタンをクリックした際に呼ばれるイベント
 	/// ゲームシーンをロードする
 	/// </summary>
-	private void OnClick()
+	public void OnClick()
 	{
-		if (gameScene != null) {
-			SceneManager.LoadScene (gameScene.buildIndex);
-		}
+		SceneManager.LoadScene ("Game");
 	}
 
 }
